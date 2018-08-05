72d98f03dcada1b122b441814ac5f1014ab488e1
Jujiva <36825142+Jujiva@users.noreply.github.com>
2018-08-05 18:56:02 -0300

    Add files via upload

---
+++ valida.c
@@ -0,0 +1,57 @@
+#include "valida.h"
+
+
+void error(int indice, char *errstr){
+    fprintf(stderr, "Indice %d:\n", indice);
+    fprintf(stderr, "%s\n", errstr);
+}
+
+/*  procesar_texto: revisa la cantidad de lineas en el texto del subtitulo,
+    la cantidad de caracteres por línea, y devuelve el total de caracteres.
+*/
+
+int procesar_texto (char* text, int indice){
+    int total_carac = 0, saltos = 0, carac_linea = 0, nueva_linea = 0;
+    for(int i = 0; text[i] != '\0'; i++){
+        total_carac++;
+        if (text[i] == '\n'){
+            salto++;
+            nueva_linea = 1;
+        }
+        if !(nueva_linea){
+            carac_linea++;
+        } else {
+            if (carac_linea > 36){
+                error(indice, "La linea tiene mas de 36 caracteres.\n");
+            }
+            carac_linea = nueva_linea = 0;
+        }
+    }
+    if (salto > 2){
+        error(indice, "El subtitulo tiene mas de 2 lineas.\n");
+    }
+
+    return total_carac;
+}
+
+int solapados_sub(size_t fin_prev, size_t ini_sig){
+    return (fin_prev >= ini_sig);
+}
+
+int separacion_sub(size_t fin_prev, size_t ini_sig){
+    return ((ini_sig - fin_prev) >= 75);
+}
+
+int minimo_duracion_sub(size_t ini, size_t fin){
+    return ((fin - ini) > 999);
+}
+
+int maximo_duracion_sub(size_t ini, size_t fin){
+    if ((fin - ini) < 7000){
+        return 1;
+    }
+    return 0;
+}
+int chars_por_seg(int total_carac, size_t duracion){
+
+}
